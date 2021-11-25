9194a9304ab6c23a822798dd71e405a8bb473649
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-11-25 11:16:52 -0600

    Create BookBlazor.cs

---
+++ TiendaProducto_Server/Models/BookBlazor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaProducto_Server.Models
+{
+    public class BookBlazor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool Is_InStock { get; set; }
+    }
+}
