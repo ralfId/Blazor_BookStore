e43dadc535e6bce275d219d588c97b24c276640e
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-11-25 15:08:51 -0600

    Create BookBenefits.cs

---
+++ TiendaProducto_Server/Models/BookBenefits.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaProducto_Server.Models
+{
+    public class BookBenefits
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
