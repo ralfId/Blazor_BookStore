28e566bced99af2faeb8c9b8ab6d6ae224f86946
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-11-25 18:28:59 -0600

    Create BookDto and data annotations

---
+++ Models/BookDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Type a name for the book")]
+        public string Name { get; set; }
+        [Range(5,100)]
+        public double Regular_price { get; set; }
+        [Required(ErrorMessage = "Type a name for author´s book")]
+        public string Author { get; set; }
+        public string Details { get; set; }
+    }
+}
