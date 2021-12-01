f3d8fbb9ec56fde96e081547acd509ccd20f462f
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-01 13:14:58 -0600

    upload file service

---
+++ TiendaProducto_Server/Services/FileUpload.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using TiendaProducto_Server.Services.IService;
+
+namespace TiendaProducto_Server.Services
+{
+    public class FileUpload : IFileUpload
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileUpload(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public async Task<string> UploadFileAsync(IBrowserFile file)
+        {
+            try
+            {
+                FileInfo fileInfo = new FileInfo(file.Name);
+                var fileName = Guid.NewGuid().ToString() + fileInfo.Extension;//file name
+                var folderDirectory = $"{_webHostEnvironment.WebRootPath}\\ImageBooks";
+                var path = Path.Combine(_webHostEnvironment.WebRootPath, "ImageBooks", fileName);
+                var memoryStream = new MemoryStream();
+
+                //create folder for book images if not exist
+                if (!Directory.Exists(folderDirectory))
+                {
+                    Directory.CreateDirectory(folderDirectory);
+                }
+
+                await using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+                {
+                    memoryStream.WriteTo(fs);
+                }
+
+                //return a full path
+                return $"ImageBooks/{fileName}";
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool DeleteFile(string fileName)
+        {
+            try
+            {
+                var filePath = $"{_webHostEnvironment.WebRootPath}\\ImageBooks\\{fileName}";
+
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ TiendaProducto_Server/Services/IService/IFileUpload.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaProducto_Server.Services.IService
+{
+    public interface IFileUpload
+    {
+        Task<string> UploadFileAsync(IBrowserFile file);
+        bool DeleteFile(string fileName);
+    }
+}
