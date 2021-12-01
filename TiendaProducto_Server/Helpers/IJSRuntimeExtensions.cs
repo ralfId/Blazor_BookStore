1844bc3d5bda95e54840d54698bad21bd4e5244a
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-01 10:05:28 -0600

    extending IJSRuntime with toastr js messages

---
+++ TiendaProducto_Server/Helpers/IJSRuntimeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaProducto_Server.Helpers
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async ValueTask ToastSuccess(this IJSRuntime JsRuntime, string success_message)
+        {
+            await JsRuntime.InvokeVoidAsync("ToastrSuccess", success_message);
+        }
+
+        public static async ValueTask ToastWarning(this IJSRuntime JsRuntime, string warning_message)
+        {
+            await JsRuntime.InvokeVoidAsync("ToastrWarning", warning_message);
+        }
+
+        public static async ValueTask ToastError(this IJSRuntime JsRuntime, string error_message)
+        {
+            await JsRuntime.InvokeVoidAsync("ToastrError", error_message);
+        }
+    }
+}
---
+++ TiendaProducto_Server/wwwroot/js/common.js
@@ -0,0 +1,11 @@
+﻿window.ToastrSuccess = (message) => {
+    toastr.success(message);
+};
+
+window.ToastrWarning = (message) => {
+    toastr.warning(message);
+};
+
+window.ToastrError = (message) => {
+    toastr.error(message);
+};
