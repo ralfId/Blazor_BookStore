c4d14f1a3df51ab502f032098c853c97d746b9de
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-04 09:59:58 -0600

    add Identity.EntityFrameworkCore

--- TiendaProducto_Server/Startup.cs
+++ TiendaProducto_Server/Startup.cs
@@ -62,9 +62,12 @@ namespace TiendaProducto_Server
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
