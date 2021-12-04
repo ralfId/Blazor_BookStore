f9004becf732cd5c2dcb7ec623b368dd97ee9634
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-04 13:14:57 -0600

    Init DB

---
+++ TiendaProducto_Server/Services/IService/IDbInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaProducto_Server.Services.IService
+{
+    public interface IDbInitializer
+    {
+        void Initializer();
+    }
+}
