eb9eee9eb84198e5fb737c595911fd1f70b3b254
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-06 21:12:44 -0600

    Create AccountController.cs

---
+++ TiendaProducto_Api/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using DataAccess.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaProducto_Api.Controllers
+{
+    [Route("api/[controler]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppUser> _roleManager;
+
+        public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager, RoleManager<AppUser> roleManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+    }
+}
