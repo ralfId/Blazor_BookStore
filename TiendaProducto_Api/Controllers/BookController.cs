844974ae60937a423b21bd7fac1966296a6aadfd
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-06 20:41:13 -0600

    get book list, get book by id

---
+++ TiendaProducto_Api/Controllers/BookController.cs
@@ -0,0 +1,59 @@
+﻿using Business.Repositories.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaProducto_Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class BookController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBooks()
+        {
+            var allBooks = await _bookRepository.GetAllBooksAsync();
+
+            return Ok(allBooks);
+
+        }
+
+        [HttpGet("{bookId}")]
+        public async Task<IActionResult> GetBookById(int? bookId)
+        {
+            if (bookId == null)
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    Title = "",
+                    ErrorMessage = "The id of the book is invalid",
+                    StatusCode = StatusCodes.Status400BadRequest
+                });
+            }
+
+            var bookDetail = await _bookRepository.GetBookByIdAsync(bookId.Value);
+
+            if (bookDetail == null)
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    Title = "",
+                    ErrorMessage = "No results found for the specified book ID",
+                    StatusCode = StatusCodes.Status404NotFound
+                });
+            }
+
+            return Ok(bookDetail);
+        }
+    }
+}
