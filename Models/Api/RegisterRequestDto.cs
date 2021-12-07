a665e826365f6981607b82df4652405c4bbae101
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-06 22:38:22 -0600

    endpoint to register new user

--- Models/Api/RegisterRequestDto.cs
+++ Models/Api/RegisterRequestDto.cs
@@ -16,7 +16,7 @@ namespace Models.Api
         [RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid {0}")]
         public string Email { get; set; }
 
-        public string Phone { get; set; }
+        public string PhoneNo { get; set; }
 
         [Required(ErrorMessage = "{0} is required")]
         public string Password { get; set; }
--- TiendaProducto_Api/Controllers/AccountController.cs
+++ TiendaProducto_Api/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
-﻿using DataAccess.Data;
+﻿using Common;
+using DataAccess.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Models.Api;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,13 +18,64 @@ namespace TiendaProducto_Api.Controllers
     {
         private readonly SignInManager<AppUser> _signInManager;
         private readonly UserManager<AppUser> _userManager;
-        private readonly RoleManager<AppUser> _roleManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager, RoleManager<AppUser> roleManager)
+        public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _signInManager = signInManager;
             _userManager = userManager;
             _roleManager = roleManager;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser([FromBody] RegisterRequestDto registerData)
+        {
+            //Check for valid data
+            if (registerData == null || !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            //new user
+            var newUser = new AppUser()
+            {
+                Name = registerData.Name,
+                Email = registerData.Email,
+                PhoneNumber = registerData.PhoneNo,
+                EmailConfirmed = true
+            };
+
+            //create new user
+            var resultCreateUser = await _userManager.CreateAsync(newUser, registerData.Password);
+
+            //if there are errors when creating the new user
+            if (!resultCreateUser.Succeeded)
+            {
+                var errors = resultCreateUser.Errors.Select(x => x.Description);
+                return BadRequest(new RegisterResponseDto
+                {
+                    IsRegistered = false,
+                    Errors = errors
+                });
+            }
+
+            //adding a role to the new user
+            var enroleUser = await _userManager.AddToRoleAsync(newUser, ConstantsCommon.Role_Client);
+
+            //if there are errors when adding a role to the new user
+            if (!enroleUser.Succeeded)
+            {
+                var errors = resultCreateUser.Errors.Select(x => x.Description);
+                return BadRequest(new RegisterResponseDto
+                {
+                    IsRegistered = false,
+                    Errors = errors
+                });
+            }
+
+            //successful response
+            return StatusCode(201);
         }
     }
 }
