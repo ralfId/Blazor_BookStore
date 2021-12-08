cf06e8745c5c6a476e3a59b5f10d51aa783d44a3
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-07 23:13:25 -0600

    cofiguring Bearer Authentication in api startup

--- TiendaProducto_Api/Startup.cs
+++ TiendaProducto_Api/Startup.cs
@@ -1,6 +1,7 @@
 using Business.Repositories;
 using Business.Repositories.IRepository;
 using DataAccess.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,10 +12,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using TiendaProducto_Api.Helpers;
 
@@ -35,8 +38,33 @@ namespace TiendaProducto_Api
             services.AddDbContext<AppDBContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppDBContext>().AddDefaultTokenProviders();
 
-            var apiSettings = Configuration.GetSection("APISettings");
-            services.Configure<APISettings>(apiSettings);
+            var apiSettingsSection = Configuration.GetSection("APISettings");
+            services.Configure<APISettings>(apiSettingsSection);
+
+            //cofiguring Bearer Authentication
+            var apiSettings = apiSettingsSection.Get<APISettings>();
+            var key = Encoding.ASCII.GetBytes(apiSettings.SecretKey);
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false,
+                x.SaveToken = true,
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidAudience = apiSettings.ValidAudience,
+                    ValidIssuer = apiSettings.ValidAudience,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddScoped<IBookRepository, BookRepository>();
