461f1fbd2d77a28406e2c4c07866fa03504c55ed
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-09 22:22:15 -0600

    Blazored LocalStorage

--- Common/ConstantsCommon.cs
+++ Common/ConstantsCommon.cs
@@ -11,5 +11,9 @@ namespace Common
         public const string Role_Admin = "Admin";
         public const string Role_Client = "Client";
         public const string Role_Employee = "Employee";
+
+        //LOCAL STORAGE
+        public const string Initial_Author = "Initial_Author";
+        
     }
 }
