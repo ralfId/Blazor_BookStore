8ad1e49291aed7dddf2d20a509a4894b840a865d
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-09 21:34:44 -0600

    adding toastr and swal messages

---
+++ TiendaProducto_Cliente/wwwroot/js/common.js
@@ -0,0 +1,46 @@
+﻿//Toastr messages
+window.ToastrSuccess = (message) => {
+    toastr.success(message);
+};
+
+window.ToastrWarning = (message) => {
+    toastr.warning(message);
+};
+
+window.ToastrError = (message) => {
+    toastr.error(message);
+};
+
+//SweetAlert messages
+window.SwalSuccess = (message) => {
+    Swal.fire(
+        'successful action!',
+        message,
+        'success'
+    )
+};
+
+window.SwalWarning = (message) => {
+    Swal.fire(
+        'Warning!',
+        message,
+        'warning'
+    )
+};
+
+window.SwalError = (message) => {
+    Swal.fire(
+        'failed action!',
+        message,
+        'error'
+    )
+};
+
+//modal confirm remove item
+window.ShowModalDeleteConfirmation = () => {
+    $('#ModalDeleteConfirmation').modal('show');
+};
+
+window.HideModalDeleteConfirmation = () => {
+    $('#ModalDeleteConfirmation').modal('hide');
+};
