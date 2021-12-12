7efaf5b18c39d1d9e859c15992fe61cac183c526
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-12 10:51:05 -0600

    Service to get books in client

---
+++ TiendaProducto_Cliente/Services/BookService.cs
@@ -0,0 +1,42 @@
+﻿using Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TiendaProducto_Cliente.Services.IService;
+
+namespace TiendaProducto_Cliente.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly HttpClient _httpClient;
+        public BookService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<BookDto> GetBookAsync(int bookId, string author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<BookDto>> GetBooksByAuthorAsync(string author)
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync($"api/book?author={author}");
+                var content = await response.Content.ReadAsStringAsync();
+                
+                return JsonConvert.DeserializeObject<IEnumerable<BookDto>>(content);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                return null;
+            }
+        }
+    }
+}
---
+++ TiendaProducto_Cliente/Services/IService/IBookService.cs
@@ -0,0 +1,14 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaProducto_Cliente.Services.IService
+{
+    public interface IBookService
+    {
+        public Task<IEnumerable<BookDto>> GetBooksByAuthorAsync(string author);
+        public Task<BookDto> GetBookAsync(int bookId, string author);
+    }
+}
