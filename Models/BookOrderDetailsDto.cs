8d97ccb04ef9b412c081de5a14f7cf6a24cf920a
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-14 10:59:06 -0600

    Create BookOrderDetailsDto.cs

---
+++ Models/BookOrderDetailsDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class BookOrderDetailsDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public string StripeSessionId { get; set; }
+        [Required]
+        public long TotalCost { get; set; }
+        [Required]
+        public int BookId { get; set; }
+        public bool IsPaid { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string PhoneNo { get; set; }
+        public BookDto BookDto { get; set; }
+        public string Status { get; set; }
+    }
+}
