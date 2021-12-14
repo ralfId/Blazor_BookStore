9dec6cafbf5e76577727badc88ead2c3253e30f9
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-14 17:39:50 -0600

    create StripePaymentService

---
+++ TiendaProducto_Cliente/Services/IService/IStripePaymentService.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaProducto_Cliente.Services.IService
+{
+    public interface IStripePaymentService
+    {
+        public Task<SuccessModel> CheckoutAsync(StripePaymentDto paymentDto);
+    }
+}
---
+++ TiendaProducto_Cliente/Services/StripePaymentService.cs
@@ -0,0 +1,29 @@
+﻿using Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TiendaProducto_Cliente.Services.IService;
+
+namespace TiendaProducto_Cliente.Services
+{
+    public class StripePaymentService : IStripePaymentService
+    {
+        private readonly HttpClient _client;
+
+        public StripePaymentService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<SuccessModel> CheckoutAsync(StripePaymentDto paymentDto)
+        {
+            var content = JsonConvert.SerializeObject(paymentDto);
+            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync
+        }
+    }
+}
