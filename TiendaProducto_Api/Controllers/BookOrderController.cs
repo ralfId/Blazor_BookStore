391f4bebf6558aa7bf4c3f40577f13423c06716d
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-14 18:11:58 -0600

    Create BookOrderController.cs

---
+++ TiendaProducto_Api/Controllers/BookOrderController.cs
@@ -0,0 +1,37 @@
+﻿using Business.Repositories.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Models.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaProducto_Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class BookOrderController : Controller
+    {
+        private readonly IBookOrderDetailsRepository _orderDetailsRespository;
+        public BookOrderController(IBookOrderDetailsRepository orderDetailsRespository)
+        {
+            _orderDetailsRespository = orderDetailsRespository;
+        }
+        public async Task<IActionResult> Create([FromBody] BookOrderDetailsDto detailsDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _orderDetailsRespository.CreateOrderAsync(detailsDto);
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    ErrorMessage = "error creating order detail",
+                });
+            }
+        }
+    }
+}
