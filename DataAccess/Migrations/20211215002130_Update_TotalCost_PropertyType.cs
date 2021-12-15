82c94c297b7348db06f1c90e8a1d51cf12d52f23
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-14 18:24:10 -0600

    update TotalCost property type

---
+++ DataAccess/Migrations/20211215002130_Update_TotalCost_PropertyType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class Update_TotalCost_PropertyType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "TotalCost",
+                table: "BookOrderDetails",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "TotalCost",
+                table: "BookOrderDetails",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
