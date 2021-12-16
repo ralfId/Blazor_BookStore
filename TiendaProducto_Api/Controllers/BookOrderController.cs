e979bb8a4d6d366cd26009d296b3625b9e0bc856
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-15 22:43:20 -0600

    endpoint for successful payment

--- TiendaProducto_Api/Controllers/BookOrderController.cs
+++ TiendaProducto_Api/Controllers/BookOrderController.cs
@@ -1,7 +1,9 @@
 ﻿using Business.Repositories.IRepository;
+using Common;
 using Microsoft.AspNetCore.Mvc;
 using Models;
 using Models.Api;
+using Stripe.Checkout;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +34,35 @@ namespace TiendaProducto_Api.Controllers
                 return BadRequest(new ErrorModel()
                 {
                     ErrorMessage = "error creating order detail",
+                });
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MarkSuccessfulPayment([FromBody] BookOrderDetailsDto detailsDto)
+        {
+            var stripeSessionService = new SessionService();
+            var stripeSessionDetails = stripeSessionService.Get(detailsDto.StripeSessionId);
+
+            if (stripeSessionDetails.PaymentStatus == "paid")
+            {
+                var result = await _orderDetailsRespository.MarkSuccessfulPaymentAsync(detailsDto.Id);
+
+                if (result == null)
+                {
+                    return BadRequest(new ErrorModel()
+                    {
+                        ErrorMessage = "Payment cannot be processed",
+                    });
+                }
+
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(new ErrorModel()
+                {
+                    ErrorMessage = "Payment cannot be processed",
                 });
             }
         }
