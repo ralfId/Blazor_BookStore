2dcdb3a038575fe0e3a2ecc2a877f76562a99585
Rafael Ibañez Diego <49356385+ralfId@users.noreply.github.com>
2021-12-16 12:34:59 -0600

    Update Register.razor
    add codebehind

---
+++ TiendaProducto_Cliente/Pages/Auth/Register.razor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using Models.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TiendaProducto_Cliente.Services.IService;
+
+namespace TiendaProducto_Cliente.Pages.Auth
+{
+    public partial class Register
+    {
+        [Inject]
+        public IAuthService authService { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        private RegisterRequestDto registerReqForm { get; set; } = new RegisterRequestDto();
+        private bool IsBusy { get; set; }
+        private bool ShowErrors { get; set; }
+        private IEnumerable<string> ErrorsLst { get; set; }
+
+        private async Task RegisterUser()
+        {
+            IsBusy = true;
+            ShowErrors = false;
+
+            var registerResp = await authService.RegisterAsync(registerReqForm);
+
+            if (registerResp.IsRegistered)
+            {
+                IsBusy = false;
+                ShowErrors = false;
+                NavigationManager.NavigateTo("/login");
+            }
+            else
+            {
+                IsBusy = false;
+                ErrorsLst = registerResp.Errors;
+                ShowErrors = true;
+            }
+        }
+    }
+}
